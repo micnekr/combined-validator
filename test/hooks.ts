dab84eb191d334ac7bd6ae54b873d0d98af1cc80
micnekr <misha.nekrasov@gmail.com>
2021-07-24 09:27:37 +0100

    Changed the licence to MIT

--- test/hooks.ts
+++ test/hooks.ts
@@ -1,22 +1,3 @@
-/*
-combined-validator - A parser for a unified format for validation of both front-end and back-end
-
-Copyright (C) 2021  micnekr
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
-
 import * as sinon from "sinon";
 
 // Restores the default sandbox after every test
